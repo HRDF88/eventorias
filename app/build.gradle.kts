487bfd125542d304e1b28cc369edc057d396d1b2
HRDF88 <julien.seguin@hotmail.fr>
2025-05-09 11:13:12 +0200

    09/05/2025, test CI2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,8 @@ sonarqube {
         property("sonar.projectName", "eventorias") // Nom de  projet SonarCloud
         property("sonar.host.url", "https://sonarcloud.io") // URL de SonarCloud
         property("sonar.login", project.findProperty("sonar.token") ?: "") // Token généré dans SonarCloud
+        property("sonar.junit.reportPaths", "build/test-results/testDebugUnitTest")
+        property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/testDebugUnitTestCoverage/testDebugUnitTestCoverage.xml")
     }
 }
 
