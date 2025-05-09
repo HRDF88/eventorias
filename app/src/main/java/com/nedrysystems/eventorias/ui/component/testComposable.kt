5793be723724d8f55780ed47576b3ae394a6ed2b
HRDF88 <julien.seguin@hotmail.fr>
2025-05-09 11:01:13 +0200

    09/05/2025, test CI

---
+++ app/src/main/java/com/nedrysystems/eventorias/ui/component/testComposable.kt
@@ -0,0 +1,9 @@
+package com.nedrysystems.eventorias.ui.component
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ComposableTest(){
+    Text(text="test")
+}
